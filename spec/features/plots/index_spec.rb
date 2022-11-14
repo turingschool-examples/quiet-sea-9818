3fdfa0532d7f182996b471b37f54565aac5ff1a2
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-14 10:52:34 -0700

    Add test for removing a plant from plot in plot index

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -10,6 +10,8 @@ RSpec.feature "Plots Index Page", type: :feature do
     @plant_1 = @plot_1.plants.create!(name: 'Cherry Tomato', description: 'Bears small, sweet tomatoes.', days_to_harvest: 90)
     @plant_2 = @plot_1.plants.create!(name: 'Green Bell Pepper', description: 'Medium sized bell peppers.', days_to_harvest: 120)
     @plant_3 = @plot_2.plants.create!(name: 'Cabbage', description: 'Leafy green.', days_to_harvest: 120)
+    
+    @plant_plot = PlantPlot.create!(plot_id: @plot_3.id, plant_id: @plant_1.id)
   end
   describe 'when visiting the page' do
     it 'lists all plot numbers' do
@@ -27,6 +29,29 @@ RSpec.feature "Plots Index Page", type: :feature do
         expect(page).to have_content('Cherry Tomato')
         expect(page).to have_content('Green Bell Pepper')
         expect(page).not_to have_content('Cabbage')
+      end
+    end
+
+    it 'has a link next to each plant that removes the plant from the plot it is under' do
+      visit plots_path
+
+      within "#plot_#{@plot_3.id}" do
+        expect(page).to have_content('Cherry Tomato')
+      end
+
+      within "#plot_#{@plot_1.id}" do
+        expect(page).to have_content('Cherry Tomato')
+      end
+      
+      within "#plot_#{@plot_1.id}" do
+        within "#plant_#{@plant_1.id}" do
+          click_link 'Remove'
+        end
+        expect(page).not_to have_content('Cherry Tomato')
+      end
+
+      within "#plot_#{@plot_3.id}" do
+        expect(page).to have_content('Cherry Tomato')
       end
     end
   end
