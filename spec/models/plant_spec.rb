0dfa7c2f4b8be5e101052acdef72d0be129e9c2f
Madeline Mauser <madeline.mauser@gmail.com>
2022-11-14 09:20:48 -0700

    Add plant model test for relationships

---
+++ spec/models/plant_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Plant, type: :model do
+  describe 'relationships' do
+    it 'relationships' do
+      it { should have_many :plant_plots}
+      it { should have_many(:plots).through(:plant_plots) }
+    end
+  end
+end
