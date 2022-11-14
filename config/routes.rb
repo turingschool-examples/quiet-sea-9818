37243b66fc74ebe70bfc774480afb6ad21f919d8
Amanda Ross <amikaross@gmail.com>
2022-11-14 09:38:28 -0700

    Test/Feat: completes user story 1, creates plots index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gardens, only: [:show] do 
+    resources :plots, only: [:index]
+  end
 end
---
+++ spec/features/plots/index_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe "the Plots Index page" do 
+  before(:each) do 
+    @garden = Garden.create!(name: "Cole Neighborhood Garden", organic: false)
+    @plot_1 = @garden.plots.create!(number: 1, size: "Large", direction: "East")
+    @plot_2 = @garden.plots.create!(number: 2, size: "Medium", direction: "West")
+    @hibiscus = Plant.create!(name: "Hibiscus", description: "Tropical Flower", days_to_harvest: 50)
+    @corn = Plant.create!(name: "Corn", description: "Your basic corn", days_to_harvest: 95)
+    @sage = Plant.create!(name: "Desert Sage", description: "The common and delicios desert sage", days_to_harvest: 36)
+    @hops = Plant.create!(name: "Vine Hops", description: "Fast growing hops vine", days_to_harvest: 12)
+
+    PlantPlot.create!(plant: @hibiscus, plot: @plot_1)
+    PlantPlot.create!(plant: @corn, plot: @plot_1)
+    PlantPlot.create!(plant: @sage, plot: @plot_1)
+    PlantPlot.create!(plant: @hops, plot: @plot_2)
+
+    other_garden = Garden.create!(name: "Whittier Garden", organic: true)
+    plot_3 = other_garden.plots.create!(number: 3, size: "Small", direction: "North")
+  end
+
+  describe "As a visitor when I visit /plots" do 
+    it "displays a list of all the plot numbers, and all that plots' plants" do 
+      visit garden_plots_path(@garden)
+
+      within "#plot-#{@plot_1.id}" do 
+        expect(page).to have_content("Plot Number: 1")
+        expect(page).to have_content("Hibiscus")
+        expect(page).to have_content("Corn")
+        expect(page).to have_content("Desert Sage")
+      end
+
+      within "#plot-#{@plot_2.id}" do 
+        expect(page).to have_content("Plot Number: 2")
+        expect(page).to have_content("Vine Hops")
+      end
+
+      expect(page).to_not have_content("Plot Number: 3")
+    end
+  end
+end
