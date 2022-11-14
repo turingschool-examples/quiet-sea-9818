7a660047bd15e538150f739377767e119d47c25f
Eli Fuchsman <efuchsman@gmail.com>
2022-11-14 11:52:01 -0700

    Complete model method for extension

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -11,4 +11,11 @@ class Garden < ApplicationRecord
   def fast_harvest_plants
     plants.distinct.where("days_to_harvest < 100").pluck(:name).join(", ")
   end
+
+  def sort_plants_by_count
+    plants.select('plants.*, count(plants.id) as plant_count')
+      .group(:id)
+      .where("days_to_harvest < 100")
+      .order('plant_count desc')
+  end
 end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -34,5 +34,18 @@ RSpec.describe Garden do
         expect(@garden.fast_harvest_plants).to eq("Plant1, Plant2, Plant3")
       end
     end
+
+    describe "#sort_plants_by_count" do
+      it "list of plants sorted by the number of plants that appear in any of that garden's plots from most to least" do
+        pp6 = PlotPlant.create!(plot: @plot1, plant: @plant1)
+        pp7 = PlotPlant.create!(plot: @plot2, plant: @plant1)
+        pp8 = PlotPlant.create!(plot: @plot2, plant: @plant1)
+        pp9 = PlotPlant.create!(plot: @plot1, plant: @plant2)
+        pp10 = PlotPlant.create!(plot: @plot1, plant: @plant2)
+        pp11 = PlotPlant.create!(plot: @plot2, plant: @plant3)
+
+        expect(@garden.sort_plants_by_count).to eq([@plant1, @plant2, @plant3])
+      end
+    end
   end
 end
