46abbe1862a33a7011c4a56d1204a22a5b66280f
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-14 11:59:15 -0700

    did what I could

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,10 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def uniq_plants_short_harvest
+     plots.joins(:plant_plots)
+          .select("plants.name")
+          .group("plants.name").where("plants.days_to_harvest < 100")
+          .distinct
+  end
 end
