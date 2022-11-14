1fbd2400afc757a28ed1214e5715721fe4bd05a6
Mostafa Sakr <msakr3@gmail.com>
2022-11-14 11:21:11 -0700

    Finish US 3

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -20,6 +20,8 @@ RSpec.describe 'gardens show page' do
   it "displays a unique list of plants with harvest days under 100 in that garden's plots" do
     visit garden_path(@garden_1)
 
+    save_and_open_page
+    
     expect(page).to have_content("Plants with Harvest Days under 100:")
     expect(page).to have_content("Lily", count: 1) #there are two in the test to check duplication
     expect(page).to have_content("Daisy", count: 1)
