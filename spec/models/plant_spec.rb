1c13936eb107cb98c0f41bd12ee9cef592e6fa98
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-11-14 11:57:07 -0500

    test/feat: relationships, migrations, schema alll set up

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Plant do
-    describe 'relationships' do
-      it { should have_many(:plot_plants) }
-      it { should have_many(:plots).through(:plot_plants) }
-    end
+  describe 'relationships' do
+    it { should have_many(:plot_plants) }
+    it { should have_many(:plots).through(:plot_plants) }
   end
 end
--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Plot do
   describe 'relationships' do
     it { should belong_to(:garden) }
+
     it { should have_many(:plot_plants) }
     it { should have_many(:plants).through(:plot_plants) }
+
   end
 end
