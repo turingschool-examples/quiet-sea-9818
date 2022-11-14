b129a4024a94756362a53f717fc5d75f0077294f
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-14 09:32:58 -0700

    set up route resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :plots, only: [:index]
+
+  resources :plant_plots, only: [:destroy]
+
+  resources :gardens, only: [:show]
 end
