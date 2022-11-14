3a27b4c6381d3864853ea76da11368abde5b3c63
Rich Kaht <adamselene1776@protonmail.com>
2022-11-14 10:07:25 -0700

    Fix: Add individual within blocks for each plot on the Plot Index

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -1,29 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe 'Plot Index' do
+  before :each do
+    @eden = Garden.create(name:"Eden", organic: true)
+    @plot1 = Plot.create(number: 1, size: "Large", direction: "East", garden_id: @eden.id)
+    @plot2 = Plot.create(number: 2, size: "Large", direction: "West", garden_id: @eden.id)
+    @tomato = Plant.create(name: "Tomato", description: "Likes sun", days_to_harvest: 110)
+    @zucchini = Plant.create(name: "Zucchini", description: "Only good in bread", days_to_harvest: 90)
+    @squash = Plant.create(name: "Squash", description: "Does anyone even eat this?", days_to_harvest: 85)
+    
+    @plot1.plants << @tomato
+    @plot1.plants << @zucchini
+    @plot2.plants << @squash
+    @plot2.plants << @tomato
+    
+    visit '/plots'
+  end
+  
   describe 'As a visitor' do
     it "I see a list of all plot numbers And under each plot number I see the
     names of all that plot's plants" do
-      eden = Garden.create(name:"Eden", organic: true)
-      plot1 = Plot.create(number: 1, size: "Large", direction: "East", garden_id: eden.id)
-      plot2 = Plot.create(number: 2, size: "Large", direction: "West", garden_id: eden.id)
-      tomato = Plant.create(name: "Tomato", description: "Likes sun", days_to_harvest: 110)
-      zucchini = Plant.create(name: "Zucchini", description: "Only good in bread", days_to_harvest: 90)
-      squash = Plant.create(name: "Squash", description: "Does anyone even eat this?", days_to_harvest: 85)
-      
-      plot1.plants << tomato
-      plot1.plants << zucchini
-      plot2.plants << squash
-      
-      visit '/plots'
-      
-      within("#plots-plants") do
-        expect(page).to have_content(plot1.number)
-        expect(page).to have_content(tomato.name)
-        expect(page).to have_content(zucchini.name)
-        expect(page).to have_content(plot2.number)
-        expect(page).to have_content(squash.name)
+      save_and_open_page
+      within("#plot-#{@plot1.number}") do
+        expect(page).to have_content(@plot1.number)
+        expect(page).to have_content(@tomato.name)
+        expect(page).to have_content(@zucchini.name)
       end
+      within("#plot-#{@plot2.number}") do
+        expect(page).to have_content(@plot2.number)
+        expect(page).to have_content(@squash.name)
+        expect(page).to_not have_content(@zucchini.name)
+      end
+    end
+    
+    xit "I see a link to remove that plant from that plot When I click on that
+    link I'm returned to the plots index page And I no longer see that plant
+    listed under that plot, And I still see that plant's name under other plots
+    that it was associated with." do
+      expect(page).to have_link("Delete #{@tomato.name}")
+      
+      click_link "Delete #{@tomato.name}"
+      expect(current_path).to eq('/plots')
+      expect(page).to_not have_content(@tomato.name)
     end
   end
 end
