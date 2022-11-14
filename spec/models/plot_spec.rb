4c83a7f28fd18381d48a3a0f5b870496c3191e2b
Alex M <alexm0717@hotmail.com>
2022-11-14 08:35:33 -0800

    vaildations complete

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -1,7 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Plot do
-  describe 'relationships' do
-    it { should belong_to(:garden) }
+
+RSpec.describe(Plot) do
+  describe("relationships") do
+    it { should(belong_to(:garden)) }
+    it { should(have_many(:plot_plants)) }
+    it { should(have_many(:plants).through((:plot_plants))) }
   end
 end
