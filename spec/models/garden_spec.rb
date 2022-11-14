9be3aa3d5ac08fd24e45fa27c7b16f826dfdf805
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-11-14 09:23:10 -0700

    Create Plant and PlotPlant models, add relationships to all models, pass all testing

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Garden do
   describe 'relationships' do
-    it { should have_many(:plots) }
+    it { should have_many :plots }
+    it { should have_many(:plants).through(:plots) }
   end
 end
--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Plot do
   describe 'relationships' do
-    it { should belong_to(:garden) }
+    it { should belong_to :garden }
+    it { should have_many :plot_plants }
+    it { should have_many(:plants).through(:plot_plants) }
   end
 end
